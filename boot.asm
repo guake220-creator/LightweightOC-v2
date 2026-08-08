6f00ec73396e583e3b9c992f1437a54b461f041b
guake220-creator <guake220@gmail.com>
2026-08-08 14:56:53 +0500

    Update boot.asm

--- boot.asm
+++ boot.asm
@@ -1,33 +1,27 @@
 [org 0x7c00]
 
-    ; 1. Вход в графический режим 320x200 256 цветов
+    ; 1. Вход в графический режим 320x200 (режим 13h)
     mov ax, 0x0013
     int 0x10
 
-    ; 2. Рисуем "панель задач" - синяя полоса внизу (цвет 1)
+    ; 2. Рисуем панель задач внизу (синяя полоса)
     mov ax, 0xA000
     mov es, ax
-    mov di, 320 * 180  ; Начало полосы
-    mov cx, 320 * 20   ; Размер полосы
-    mov al, 1          ; Синий цвет
-    rep stosb
+    mov di, 320 * 180       ; Начало полосы (y = 180)
+    mov cx, 320 * 20        ; Размер полосы (высота 20 строк)
+    mov al, 1               ; Синий цвет
+    rep stosb               ; Заполняем память экрана
 
-    ; 3. Рисуем "голову" кота (белый прямоугольник 40x20)
+    ; 3. Рисуем элемент интерфейса
     mov di, 320 * 80 + 140
     mov cx, 40
-.head:
+.golova:
     mov byte [es:di+cx], 15 ; Белый цвет
     add di, 320
-    loop .head
-    
-    ; 4. Текстовая подпись
-    mov ah, 0x02
-    mov bh, 0
-    mov dh, 24
-    mov dl, 15
-    int 0x10
-    
-    mov si, msg
+    loop .golova
+
+    ; 4. Вывод текста
+    mov si, message
     mov ah, 0x0E
 .print:
     lodsb
@@ -35,10 +29,11 @@
     je .done
     int 0x10
     jmp .print
+
 .done:
     jmp $
 
-msg db 'CatOS Graphical Interface', 0
+message db "catOS Desktop", 0
 
 times 510-($-$$) db 0
 dw 0xaa55

25736df34271e7ece2e4e026daa7b21a18f0f569
guake220-creator <guake220@gmail.com>
2026-08-08 13:38:36 +0500

    boot.asm

--- boot.asm
+++ boot.asm
@@ -1,17 +1,37 @@
-[BITS 32]
-section .multiboot
-    align 4
-    dd 0x1BADB002            ; магическое число Multiboot
-    dd 0x00                  ; флаги
-    dd - (0x1BADB002 + 0x00) ; контрольная сумма
+[org 0x7c00]          ; Начальный адрес загрузки BIOS
 
-section .text
-global start
-extern kernel_main
+; 1. Переходим в текстовый режим 80x25 и очищаем экран
+mov ah, 0x00
+mov al, 0x03          
+int 0x10
 
-start:
-    cli                      ; выключаем прерывания
-    call kernel_main         ; вызываем функцию из C-файла
-.hang:
-    hlt                      ; останавливаем процессор
-    jmp .hang
+; 2. Выводим текст "catOS loading..."
+mov si, greeting_msg
+call print_string
+
+animation_loop:
+    ; 3. Выводим анимированный символ для спирали/эффекта
+    mov ah, 0x09
+    mov al, '*'       
+    mov bh, 0x00
+    mov bl, 0x0A      ; Зеленый цвет текста
+    mov cx, 1         
+    int 0x10
+
+    jmp animation_loop
+
+print_string:
+    lodsb
+    cmp al, 0
+    je .done
+    mov ah, 0x0E
+    int 0x10
+    jmp print_string
+.done:
+    ret
+
+greeting_msg db 'catOS loading...', 13, 10, 0
+
+; Заполняем до 512 байт и добавляем стандартную подпись загрузчика
+times 510-($-$$) db 0
+dw 0xaa55
